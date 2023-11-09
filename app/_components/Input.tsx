38fadb69aefa7f6379adfa3a6737ecad74aa46da
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-11-10 03:22:19 +0700

    Component Input: add disabled props

--- app/_components/Input.tsx
+++ app/_components/Input.tsx
@@ -6,14 +6,16 @@ type InputProps = {
    onChange?: React.ChangeEventHandler<HTMLInputElement>;
    id?: string;
    error?: boolean;
+   disabled?: boolean;
 }
-export default function Input({className, error, ...props} : InputProps) {
+export default function Input({className, error, disabled, ...props} : InputProps) {
    return <input 
       type="text" 
       className={
          clsx(
-            "block w-full bg-transparent border-2 border-gray-500 focus:border-primary outline-0 rounded py-2 px-4 text-xs font-semibold", 
+            "block w-full bg-transparent border-2 border-gray-500 focus:border-primary outline-0 rounded py-2.5 px-4 text-xs font-semibold", 
             error ? 'border-red-500 focus:border-red-500' : '',
+            disabled ? 'opacity-30' : '',
             className
          )
       } 
