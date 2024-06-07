0a32cb45b2b9e7b5d9674e307b7832d78892b5db
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-06-07 13:51:53 +0700

    style, feat, component: modal on mobile view will have full width/height with max 480px and for the Modal component add close button on modal view

--- app/_components/Modal.tsx
+++ app/_components/Modal.tsx
@@ -3,10 +3,11 @@ import Modal from 'react-modal'
 import clsx from 'clsx'
 import { useSelector } from 'react-redux';
 import { RootState } from '../_redux/store';
+import XIcon from '../_assets/icons/x.svg';
 // (event: MouseEvent<Element, MouseEvent> | KeyboardEvent<Element>): void
 export type CustomModalProps = {
    isOpen: boolean,
-   onRequestClose: React.MouseEventHandler<Element> | React.KeyboardEventHandler<Element>,
+   onRequestClose: React.MouseEventHandler<Element>,
    children: React.ReactElement,
 }
 export default function CustomModal({isOpen, onRequestClose, children} : CustomModalProps) {
@@ -25,7 +26,17 @@ export default function CustomModal({isOpen, onRequestClose, children} : CustomM
          shouldCloseOnOverlayClick={true}
          shouldCloseOnEsc={true}
          onRequestClose={onRequestClose}
-         contentElement={(props, children) => <div onClick={props.onClick} className={clsx(props.className, 'absolute z-[1100] top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[480px] bg-white dark:bg-dark-light p-8 rounded-lg')}>{children}</div>}
+         contentElement={(props, children) => <div onClick={props.onClick} className={clsx(props.className, 'absolute z-[1100] top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 max-w-[480px] mobile:w-full mobile:h-full bg-white dark:bg-dark-light p-8 rounded-lg')}>
+            <>
+               <button
+                  className='absolute top-0 right-0 p-2 bg-dark md:hidden'
+                  onClick={(e: React.MouseEvent<Element>) => onRequestClose(e)}
+               >
+                  <XIcon className="text-slate-400"/>
+               </button>
+               {children}
+            </>
+         </div>}
       >
          {children}
       </Modal>
